8af6cec8d447294c999ca7b1c147e37eb9c9709c
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-08-23 10:18:48 +0300

    sum start

--- otus-lesson-28/Program.cs
+++ otus-lesson-28/Program.cs
@@ -4,9 +4,21 @@
 
 Console.WriteLine("Hello");
 
+Console.WriteLine(SummMassiv(3));
 
 
 
+static int SummMassiv (int n)
+{
+    int Summ = 0; 
+    var massiv = new int[n];
+
+
+    for (int i = 0; i < n; i++) { massiv[i] = i; }
+
+
+    return massiv.Sum();
+}
 
 
 Console.ReadKey();
