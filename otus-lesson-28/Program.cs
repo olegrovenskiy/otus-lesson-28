f52bed4661e677e2582cdcabe0aecefb397c9d53
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-08-23 16:51:47 +0300

    fin

--- otus-lesson-28/Program.cs
+++ otus-lesson-28/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 
 
 Console.WriteLine("Hello");
+Console.WriteLine("");
+Console.WriteLine("");
 
-int n1 = 1000;
-int n2 = 10000;
+
+int n1 = 100000;
+int n2 = 1000000;
 int n3 = 10000000;
 
 var massiv1 = new long[n1];
@@ -40,6 +44,7 @@ SummMassiv(massiv3);
 stopwatch.Stop();
 Console.WriteLine("Время выполнения  обычно  " + stopwatch.ElapsedMilliseconds + "  мсек");
 
+Console.WriteLine("----------------------------------------");
 
 
 
@@ -60,7 +65,7 @@ SummMassivParallel(massiv3);
 stopwatch.Stop();
 Console.WriteLine("Время выполнения  пар  " + stopwatch.ElapsedMilliseconds + "  мсек");
 
-
+Console.WriteLine("----------------------------------------");
 
 stopwatch.Start();
 SummMassivPLinq(massiv1);
@@ -80,6 +85,24 @@ stopwatch.Stop();
 Console.WriteLine("Время выполнения  PL  " + stopwatch.ElapsedMilliseconds + "  мсек");
 
 
+Console.WriteLine("----------------------------------------");
+
+stopwatch.Start();
+SummMassivThread(massiv1);
+stopwatch.Stop();
+Console.WriteLine("Время выполнения  Thr  " + stopwatch.ElapsedMilliseconds + "  мсек");
+
+
+stopwatch.Restart();
+SummMassivThread(massiv2);
+stopwatch.Stop();
+Console.WriteLine("Время выполнения  Thr  " + stopwatch.ElapsedMilliseconds + "  мсек");
+
+
+stopwatch.Restart();
+SummMassivThread(massiv3);
+stopwatch.Stop();
+Console.WriteLine("Время выполнения  Thr  " + stopwatch.ElapsedMilliseconds + "  мсек");
 
 
 
@@ -91,30 +114,79 @@ Console.WriteLine("Время выполнения  PL  " + stopwatch.ElapsedMil
 
 static long SummMassiv (long[] massiv)
 {
-    long Summ = 0; 
+    long Summ = 0;
 
-    Summ = massiv.Sum();
+    for (int i = 0; i < massiv.Count(); i++)
+    {
+        Summ = Summ + massiv[i]; }
 
-    Console.WriteLine(Summ);
+        Console.WriteLine(Summ);
     return Summ;
 }
+
+
+
+static long SummMassivThread(long[] massiv)
+{
+    long Summ1 = 0;
+    long Summ2 = 0;
+
+
+    Thread myThread1 = new Thread(() =>
+
+    {
+        for (int i = 0; i < massiv.Count()/2; i++)
+        {
+            Summ1 = Summ1 + massiv[i];
+        }
+
+    });
+
+
+    Thread myThread2 = new Thread(() =>
+
+    {
+        for (int i = massiv.Count() / 2; i < massiv.Count(); i++)
+        {
+            Summ2 = Summ2 + massiv[i];
+        }
+
+    });
+
+    myThread1.Start();
+    myThread2.Start();
+
+    myThread1.Join();
+    myThread2.Join();
+
+    Console.WriteLine(Summ1+Summ2);
+    return Summ1+Summ2;
+}
+
 
 
 
 static long SummMassivParallel(long[] massiv)
 {
     long Summ = 0;
-    
 
-    Parallel.For(0, massiv.Count(), new ParallelOptions()
-    {
-        MaxDegreeOfParallelism = 4
-    }, i =>
+    Parallel.For(0, massiv.Count(), i =>
     {
         Summ = Summ + massiv[i];
 
     });
 
+
+    /*
+    Parallel.For(0, massiv.Count(), new ParallelOptions()
+    {
+        MaxDegreeOfParallelism = 1
+    }, i =>
+    {
+        Summ = Summ + massiv[i];
+
+    });
+    */
     Console.WriteLine(Summ);
     return Summ;
 }
